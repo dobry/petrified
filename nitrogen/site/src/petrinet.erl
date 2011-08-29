83b8f915c86a5558185eca8b73c23dbdad7f0cd2
Krzysiek <adres.dobry@gmail.com>
2011-08-30 01:08:53 +0200

    Added file upload form in files menu.

--- nitrogen/site/src/petrinet.erl
+++ nitrogen/site/src/petrinet.erl
@@ -13,11 +13,15 @@ body() ->
     #panel { id = "app", body = [
       #panel { id = "floating_messages", body = #flash{} },
       #panel { id = "menus", body = 
+      [
         #dropdown { id = menu_drop, postback = menu_select, options = 
         [
           #option { text = "Pliki", value = "files" },
           #option { text = "Elementy", value = "elements" }
-        ]}},
+        ]},
+        #panel { id = menu_items, body = menu_files() }
+      ]},
+        
       #panel { id = "editor", body = "
         <canvas id=\"canvas\"></canvas>
       " }
@@ -27,3 +31,24 @@ body() ->
 event(menu_select) ->
   Menu = wf:q(menu_drop),
   wf:flash("You opened menu " ++ Menu ++ ".").
+
+% TODO make custom element of this function
+menu_files() ->
+  [
+    "Wczytaj plik",
+    #br {},
+    #upload { tag = myUpload1, button_text = "Wczytaj plik" }
+  ].
+
+start_upload_event(myUpload1) ->
+    wf:flash("Upload started.").
+
+finish_upload_event(_Tag, undefined, _, _) ->
+    wf:flash("Please select a file."),
+    ok;
+
+finish_upload_event(_Tag, FileName, LocalFileData, Node) ->
+    FileSize = filelib:file_size(LocalFileData),
+    wf:flash(wf:f("Uploaded file: ~s (~p bytes) on node ~s.", [FileName, FileSize, Node])),
+    ok.
+
