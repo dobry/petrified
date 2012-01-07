38ca3ed8e1604b2325eff9ff82e9bcdca6f1fa43
Krzysiek <adres.dobry@gmail.com>
2012-01-07 20:36:53 +0100

    Transition in spawned, init returns Pid. Next - init arcs.

--- simulation/net_records.hrl
+++ simulation/net_records.hrl
@@ -6,3 +6,14 @@
   markers = 0,
   capacity = -1 % -1 means infinite capacity
 }).
+
+-record(transition,
+{
+  id,
+  name,
+  left = 0,
+  top = 0,
+  delay,
+  arcs_to = [],
+  arcs_from = []
+}).
--- simulation/transition.erl
+++ simulation/transition.erl
@@ -1,9 +1,21 @@
 -module(transition).
--export([init/1, loop/1]).
+-export([init/1, init_arcs/1]).
 -include("net_records.hrl").
 
-init(Transition) ->
-  io:format("Transition lauched:~p~n", [Transition]),
+init(Attrs) ->
+  %io:format("Transition lauched:~p~n", [Attrs]),
+  [{<<"id">>, Id} | [{<<"name">>, Name} | [{<<"x">>, Left} | [{<<"y">>, Top} | [{<<"delay">>, Delay} | [_Angle]]]]]] = Attrs,
+  Tr = #transition {
+    id = Id,
+    name = Name,
+    left = Left,
+    top = Top,
+    delay = Delay
+  },
+  spawn(transition, init_arcs, [Tr]).
+
+init_arcs(Tr) ->
+  io:format("Transition lauched:~p~n", [Tr]),
   ok.
 
 loop(_Transition) ->
